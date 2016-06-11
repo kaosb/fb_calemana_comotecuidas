edf8cb96240f4c9182a04f1519febe3d0ac9cbdd
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2016-06-11 12:56:29 -0400

    Fix script

--- htdocs/index.php
+++ htdocs/index.php
@@ -49,7 +49,7 @@
 						$.userdata.userID = response.authResponse.userID;
 						$.userdata.accessToken = response.authResponse.accessToken;
 						// Obtengo informacion del perfil.
-						FB.api('/me', {fields: 'name,first_name,last_name,email,username'}, function(data){
+						FB.api('/me', {fields: 'name,first_name,last_name,email'}, function(data){
 							console.log(data);
 							$.userdata.name = data.name;
 							$.userdata.first_name = data.first_name;
