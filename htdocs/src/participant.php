2a66b80a92d79656dff5efd06fb5c64db21c9a29
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2016-06-11 20:58:23 -0400

    Fix script

--- htdocs/src/participant.php
+++ htdocs/src/participant.php
@@ -15,7 +15,7 @@
 				$post_id = $_POST['post_id'];
 				$queryInsert = "INSERT INTO participantes (userID, accessToken, first_name, last_name, name, email, words, template, origin, post_id)
 								VALUES ('$userID', '$accessToken', '$first_name', '$last_name', '$name', '$email', '$words', '$template', '$origin', '$post_id')";
-				$queryInsert = utf8_decode($queryInsert);				
+				$queryInsert = mysqli_real_escape_string($link, $queryInsert);				
 				if(mysqli_real_query($link, $queryInsert)){
 					// insert exitoso
 					echo json_encode(array("status" => true, "msj" => "Se guardaron los datos de forma exitosa."));
