be684e50529e3987f6542fb35b8762949f28410b
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2016-06-11 20:29:44 -0400

    Fix script

--- htdocs/src/connection.php
+++ htdocs/src/connection.php
@@ -1,6 +1,3 @@
 <?php
-	// $link = mysql_connect("localhost","calemana","calemana2016+-");
 	$link = mysqli_connect('localhost', 'calemana', 'calemana2016+-', 'fb_calemana_comotecuidas_2016');
-	mysql_set_charset('utf8mb4',$link);
-	mysql_select_db("fb_calemana_comotecuidas_2016",$link);
 ?>
