1dbbb9cb41f0469d9b2f5f31d00b74385562f428
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2016-06-19 14:49:15 -0400

    Check participacion

---
+++ htdocs/src/checkparticipant.php
@@ -0,0 +1,17 @@
+<?php
+if(isset($_POST['uid'])){
+	include("connection.php");
+	$userID = $_POST['userID'];
+	$query = "select * from participant where userID = '$userID'";
+
+	$queryExiste = mysqli_real_query($link, $query);
+	$contadorExiste = mysqli_num_rows($queryExiste);
+	if($contadorExiste < 1){
+		echo json_encode(array("status" => true, "msj" => "El usuario no a participado anteriormente."));
+	}else{
+		echo json_encode(array("status" => false, "msj" => "El usuario ya habia participado.", "data" => mysqli_fetch_assoc($queryExiste)));
+	}
+}else{
+	echo json_encode(array("status" => false, "msj" => "Parametros insuficientes para ejecutar la accion solicitada."));
+}
+?>
